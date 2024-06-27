5ebfd6234e1b2996a8e7b216c8bc6e61b01f0340
sckntrd <150478138+sckntrd@users.noreply.github.com>
2024-06-27 19:36:30 +0000

    bug fix 3# Please enter the commit message for your changes. Lines starting

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy({setTo: 'PHP/5.4.0'}));
+app.use(helmet.hidePoweredBy());
 
 
 
