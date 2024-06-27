5c6f0971023269389489704fd0b87857b1503081
sckntrd <150478138+sckntrd@users.noreply.github.com>
2024-06-27 20:07:40 +0000

    trial and error #4

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet());
-app.use(helmet.hidePoweredBy());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 
 
 
